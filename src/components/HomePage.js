6a825a8fc3620959a958773ece5eb074ee2efc58
Kianshi <kpian@vt.edu>
2024-09-14 02:44:05 -0400

    navbar try
    tried js

---
+++ src/components/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">Welcome to My Website</h1>
+      <p className="mb-4">
+        This is a basic homepage template using Tailwind CSS and React.js.
+      </p>
+      <p>
+        You can add more content here to display information about your website.
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
